10c2275390d4e017bd0a06c6d12a804fb87951e4
hong <etkimhong@gmail.com>
2024-08-21 10:06:06 +0700

    six

--- app/page.tsx
+++ app/page.tsx
@@ -47,9 +47,6 @@ export default function Home() {
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
           </h2>
         </a>
 
